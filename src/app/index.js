070929d98decad10ac55f770a8971f0c03258c60
Rahul Ravindran <rahulravindran2709@gmail.com>
2019-01-27 09:50:15 -0400

    react-router working

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,21 @@
 import * as React from 'react'
+import { Switch, NavLink,Route,BrowserRouter } from 'react-router-dom'
+import Home from './views/home'
 
-const App = () => <div>Hello</div>
+
+const App = () => <BrowserRouter><div>Hello
+  <ul>
+          <li>
+            <NavLink to="/home">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </ul>
+<Switch>
+  <Route path="/home" component={Home} />
+  <Route render={() => <div>coomon</div>}/>
+</Switch></div></BrowserRouter>
 
 
 export default App
---
+++ src/app/views/home.js
@@ -0,0 +1,4 @@
+import * as React from 'react'
+
+const Home = () => (<div>Home</div>)
+export default Home
